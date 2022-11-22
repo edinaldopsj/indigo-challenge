c309ccbee1a512d694400051157237b604420135
EdinaldoPSJ <edinaldopsj92@gmail.com>
2022-11-22 15:12:27 -0300

    Adding test and code changes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,64 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+
+test('Should render the app', () => {
+  render(<App />);
+});
 
 test('renders text input', () => {
   render(<App />);
-  const textarea = screen.getByRole('textbox');
-  expect(textarea).toBeInTheDocument();
+  const textArea = screen.getByRole('textbox');
+
+  expect(textArea).toBeInTheDocument();
+});
+
+describe('Should render specific elements based on Markdown', () => {
+  it('Should render an H1 when typing # in the first character followed by a sequence of words', async () => {
+    render(<App />);
+
+    const textArea = screen.getByTestId('markdown');
+    const preview = screen.queryByTestId('preview');
+
+    userEvent.type(textArea, '# Hey this is a title');
+
+    expect(preview).toContainHTML(`<h1>Hey this is a title</h1>`);
+  });
+
+  it('Should render an H2 when typing ## in the first character followed by a sequence of words', () => {
+    render(<App />);
+
+    const textArea = screen.getByTestId('markdown');
+    const preview = screen.queryByTestId('preview');
+
+    userEvent.type(textArea, '## Hey this is a secondary title');
+
+    expect(preview).toContainHTML(`<h2>Hey this is a secondary title</h2>`);
+  });
+
+  it('Should render a P element when only typing text without markdown identifiers', () => {
+    render(<App />);
+
+    const textArea = screen.getByTestId('markdown');
+    const preview = screen.queryByTestId('preview');
+
+    userEvent.type(textArea, 'Hey this is common paragraph');
+
+    expect(preview).toContainHTML(`<p>Hey this is common paragraph</p>`);
+  });
+
+  it('Should render a list with two elements with proper markdown typing', () => {
+    render(<App />);
+
+    const textArea = screen.getByTestId('markdown');
+    const preview = screen.queryByTestId('preview');
+
+    userEvent.type(textArea, '* My first list item\n');
+    userEvent.type(textArea, '* My second list item\n');
+
+    expect(preview).toContainHTML(
+      `<ul><li> My first list item</li><li> My second list item</li></ul>`,
+    );
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,116 @@
+import React, { ReactNode, useCallback, useState } from 'react';
+
+type RootElements = 'ul' | 'p' | 'h1' | 'h2';
+
 type Block = {
-  element: 'ul' | 'li' | 'p' | 'h1' | 'h2';
+  element: RootElements | 'li';
   children: (Block | string)[];
 };
 
+interface Render {
+  h1: (value: (Block | string)[]) => ReactNode;
+  h2: (value: (Block | string)[]) => ReactNode;
+  p: (value: (Block | string)[]) => ReactNode;
+  ul: (value: (Block | string)[]) => ReactNode;
+}
+
+const renders: Render = {
+  h1: (value) => <h1>{value[0] as string}</h1>,
+  h2: (value) => <h2>{value[0] as string}</h2>,
+  p: (value) => <p>{value[0] as string}</p>,
+  ul: (value) => (
+    <ul>
+      {(value as Block[]).map((text, index) => (
+        <li key={index}>{text.children[0] as string}</li>
+      ))}
+    </ul>
+  ),
+};
+
 const parseMarkdown = (markdown: string): Block[] => {
-  // TODO: implement
+  const blocks = markdown.split('\n\n');
+  let parsed: Block[] = [];
+
+  for (let index = 0; index < blocks.length; index++) {
+    let block = blocks[index];
+    let lines = block.split('\n');
+
+    let ulBlock: Block = {
+      element: 'ul',
+      children: [],
+    };
+
+    for (let j = 0; j < lines.length; j++) {
+      const element = lines[j];
+
+      if (element === '') {
+        continue;
+      }
+
+      const currentElement = parseElement(element);
+
+      if (currentElement.element === 'li') {
+        if (ulBlock.children.length === 0) {
+          parsed.push(ulBlock);
+        }
+
+        ulBlock.children.push(currentElement);
+      } else {
+        parsed.push(currentElement);
+      }
+    }
+  }
+
+  return parsed;
+};
+
+const parseElement = (line: string): Block => {
+  if (line.substring(0, 2) === '# ') {
+    return {
+      element: 'h1',
+      children: [line.substring(2, line.length)],
+    };
+  }
+  if (line.substring(0, 3) === '## ') {
+    return {
+      element: 'h2',
+      children: [line.substring(3, line.length)],
+    };
+  }
+  if (['*', '-'].includes(line.substring(0, 1))) {
+    return {
+      element: 'li',
+      children: [line.substring(1, line.length)],
+    };
+  }
+  return { element: 'p', children: [line] };
 };
 
 function App() {
+  const [blocks, setBlocks] = useState<Block[]>([]);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setBlocks(parseMarkdown(e.target.value));
+  };
+
+  const renderPreview = useCallback(() => {
+    return <div>{blocks.map((b) => renders[b.element as RootElements](b.children))}</div>;
+  }, [blocks]);
+
   return (
-    <div>
-      <textarea onChange={(e) => console.log(e.target.value)} rows={5} />
+    <div style={{ display: 'flex', gap: '10px' }}>
+      <div style={{ flex: 1 }}>
+        <textarea
+          data-testid="markdown"
+          placeholder="Please type the markdown text here"
+          style={{ width: '100%' }}
+          onChange={handleOnChange}
+          rows={5}
+        />
+      </div>
+      <div data-testid="preview" style={{ border: '1px solid', flex: 1 }}>
+        {renderPreview()}
+      </div>
     </div>
   );
 }
